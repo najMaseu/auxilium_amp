12a52bffb41ceaf6b41802f195052d6c9da3b43d
najmaslo <maseu@protonmail.com>
2019-12-21 17:36:52 +0100

    cra cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import anime from "animejs";
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          height: "100vh"
+        }}
+        className="App-header"
+      >
+        <div
+          className="dupa"
+          style={{
+            height: 100,
+            width: 100
+          }}
+          onClick={() => {
+            anime({
+              targets: ".dupa",
+              translateX: 250,
+              backgroundColor: ["white", "hsl(200, 50%, 50%)"],
+              duration: 1000
+            });
+          }}
+        ></div>
       </header>
     </div>
   );
-}
+};
 
 export default App;
