6ee60e79f929b0c95938e9541c136331d71b1aab
najmaslo <maseu@protonmail.com>
2020-01-26 09:29:55 +0100

    connecr audio context to guitar input and remove static audio from project

--- src/components/WebPlayer/WebPlayer.tsx
+++ src/components/WebPlayer/WebPlayer.tsx
@@ -1,11 +1,9 @@
 import React, { useRef, useEffect } from "react";
 //@ts-ignore
-import sound from "../../static/sound.wav";
 import { drawAnalyser } from "../../helpers/drawAnalyser";
 import { canvas } from "./WebPlayer.styles";
 
 export const WebPlayer: React.FC = () => {
-  const audio = useRef<HTMLMediaElement>(null);
   const drawing = useRef<HTMLCanvasElement>(null);
 
   const audioCtx = new window.AudioContext();
@@ -16,10 +14,20 @@ export const WebPlayer: React.FC = () => {
   const dataArray = new Uint8Array(analyser.frequencyBinCount);
 
   useEffect(() => {
-    const source = audioCtx.createMediaElementSource(
-      audio.current as HTMLMediaElement
-    );
-    source.connect(analyser);
+    (async () => {
+      const audioStream = await navigator.mediaDevices.getUserMedia({
+        audio: {
+          echoCancellation: false,
+          autoGainControl: false,
+          noiseSuppression: false,
+          latency: 0
+        }
+      });
+
+      const source = audioCtx.createMediaStreamSource(audioStream);
+
+      source.connect(analyser);
+    })();
 
     const canvasCtx = drawing.current!.getContext("2d");
 
@@ -27,28 +35,14 @@ export const WebPlayer: React.FC = () => {
   }, [analyser, audioCtx, dataArray]);
 
   return (
-    <>
-      <audio
-        style={{
-          position: "fixed",
-          bottom: 20,
-          left: 20
-        }}
-        ref={audio}
-        controls
-      >
-        <source src={sound} type="audio/wav" />
-      </audio>
-
-      <canvas
-        className={canvas}
-        ref={drawing}
-        id="drawing"
-        width="686"
-        height="150"
-      >
-        <p>Your browser does not support the canvas tag...</p>
-      </canvas>
-    </>
+    <canvas
+      className={canvas}
+      ref={drawing}
+      id="drawing"
+      width="686"
+      height="150"
+    >
+      <p>Your browser does not support the canvas tag...</p>
+    </canvas>
   );
 };
